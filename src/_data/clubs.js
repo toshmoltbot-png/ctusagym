1bc36b53d8dd75a28a7b6ed121c58aefcdecc6e7
Tosh Moltbot <ToshMoltBot@gmail.com>
2026-07-30 21:38:43 -0400

    Split all data collections into individual files for CloudCannon editing

---
+++ src/_data/clubs.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function() {
+  const dir = path.join(__dirname, 'clubs');
+  if (!fs.existsSync(dir)) return [];
+  const files = fs.readdirSync(dir).filter(f => f.endsWith('.json')).sort();
+  return files.map(f => JSON.parse(fs.readFileSync(path.join(dir, f), 'utf-8')));
+};
